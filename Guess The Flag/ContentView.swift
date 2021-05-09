51addfa2914f055980bec45cd3997d28ef7c178b
Gayan Kalinga <bg15407@gmail.com>
2021-05-09 19:28:28 +0530

    Initial Commit

---
+++ Guess The Flag/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Guess The Flag
+//
+//  Created by Gayan Kalinga on 5/9/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
